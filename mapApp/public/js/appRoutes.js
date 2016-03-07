6268306952e578bc83061138549761eafc9c1a66
MichaelImperia <mhodder10@gmail.com>
2016-03-07 15:35:48 +0000

    Current Frontend

--- mapApp/public/js/appRoutes.js
+++ mapApp/public/js/appRoutes.js
@@ -4,7 +4,7 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', f
 
 		// home page
 		.when('/', {
-			templateUrl: 'views/home.html',
+			templateUrl: 'views/main.html',
 			controller: 'MainController'
 		})
 
@@ -16,6 +16,11 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', f
 		.when('/win3', {
 			templateUrl: 'views/geek.html',
 			controller: 'GeekController'	
+		})
+
+		.when('/win4', {
+			templateUrl: 'views/home.html',
+			controller: 'HomeController'	
 		});
 
 	$locationProvider.html5Mode(true);
